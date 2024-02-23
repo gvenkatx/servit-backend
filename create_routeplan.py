bdb23775c93c6927651221191229c194ad7162e0
Giri Venkataraman <giri@redhat.com>
2024-02-22 21:44:48 -0500

    Updates to single file

--- create_routeplan.py
+++ create_routeplan.py
@@ -1,39 +1,46 @@
+import random
+import string
 import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials, firestore
 import json
 from google.cloud.firestore_v1 import GeoPoint
-from datetime import datetime
+from datetime import date, datetime
 import requests
 import time
-from geopy.geocoders import Nominatim
+from geopy.geocoders import GoogleV3
+from flask import Flask, request
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate('./serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
+firebase_cred = credentials.Certificate('./serviceAccountKey.json')
+fapp = firebase_admin.initialize_app(firebase_cred)
 
-# Get a reference to the Firestore database
-db = firestore.client()
+with open('./routeplanParams.json') as pfile:
+    tfold_data = json.load(pfile)
 
-# Gathering specific Document IDs for donors
-collection_donor = "customer"
-consolidated_data1 = []
-collection_refA = db.collection(collection_donor)
-document_idsA = [doc.id for doc in collection_refA.stream()]
+routeplan_datetime = datetime.utcnow()
+routeplan_day = routeplan_datetime.date()
 
-# Gathering specific Document IDs for teen drivers
-collection_driver = "teendriver"
-consolidated_data2 = []
-collection_refB = db.collection(collection_driver)
-document_idsB = [doc.id for doc in collection_refB.stream()]
+#Utility functions
+def reverse_loc(lat_long):
+    #geolocator = Nominatim(user_agent="mytestapp")
+    geolocator = GoogleV3(api_key=tfold_data['maps_api_key'])
+    location = geolocator.reverse(str(lat_long[0])+","+str(lat_long[1]))
+    return location.address
 
-consolidated_data3 = []
-collection_depot = "depot"
-collection_refC = db.collection(collection_depot)
-document_idsC = [doc.id for doc in collection_refC.stream()]
+def date_from_str(datestr):
+    return datetime.strptime(datestr,'%Y-%m-%dT%H:%M:%S').date()
 
-# Function for gathering data from Documents
-def my_function(collection_name, document_id, result_list, persona):
+def hr_min_from_seconds(seconds):
+    #minutes, seconds = divmod(seconds, 60)
+    #hours, minutes = divmod(minutes, 60)
+    #return (str(hours) + ' hr, ' + str(minutes) + ' min')
+    return(seconds//60)
+
+
+# Utility function for gathering data from firebase documents
+def read_collection(collection_name, document_id, result_list, persona):
+
+    db = firestore.client()
     # Reference to a specific document
     doc_ref = db.collection(collection_name).document(document_id)
     # Get the document snapshot
@@ -42,79 +49,180 @@ def my_function(collection_name, document_id, result_list, persona):
     if doc_snapshot.exists:
         # Access the document data
         doc_data = doc_snapshot.to_dict()
-        if persona=='customer':
+        if (persona=='customer' and doc_data['minStartTime'].date() == routeplan_day):
             hardcode_values = {'serviceDuration':1200.000000000, 'vehicle':None, 'previousCustomer':None, 'nextCustomer': None,
                                 'arrivalTime':None, 'startServiceTime':None, 'departureTime':None,
                                 'drivingTimeSecondsFromPreviousStandstill': None}
             doc_data.update(hardcode_values)
 
-        if persona=='teendriver':
+            for key in doc_data:
+                if isinstance(doc_data[key], GeoPoint):
+                    doc_data[key] = [doc_data[key].latitude, doc_data[key].longitude]
+
+                if isinstance(doc_data[key], datetime):
+                    doc_data[key] = doc_data[key].strftime("%Y-%m-%d" + "T" + "%H:%M:%S")
+
+            result_list.append(doc_data)
+
+        if (persona=='teendriver' and doc_data['departuretime'].date() == routeplan_day):
             hardcode_values = {'customers':[], 'totalDemand':0, 'totalDrivingTimeSeconds':0}
             doc_data.update(hardcode_values)
 
-        for key in doc_data:
-            if isinstance(doc_data[key], GeoPoint):
-                doc_data[key] = [doc_data[key].latitude, doc_data[key].longitude]
+            for key in doc_data:
+                if isinstance(doc_data[key], GeoPoint):
+                    doc_data[key] = [doc_data[key].latitude, doc_data[key].longitude]
 
-            if isinstance(doc_data[key], datetime):
-                doc_data[key] = doc_data[key].strftime("%Y-%m-%d" + "T" + "%H:%M:%S")
+                if isinstance(doc_data[key], datetime):
+                    doc_data[key] = doc_data[key].strftime("%Y-%m-%d" + "T" + "%H:%M:%S")
 
-        #print("Document data:", doc_data)
-        result_list.append(doc_data)
-        #return(result_list)
+            result_list.append(doc_data)
+        
 
-# Donor Code
-for doc_id in document_idsA:
-    my_function(collection_donor, doc_id, consolidated_data1, 'customer')
+#Main function to read from firebase and create route plan input
+def read_routeplan_input(routeplaninput):
+    
+    # Get a reference to the Firestore database
+    db = firestore.client()
 
-# Teen Code
-for doc_id in document_idsB:
-    my_function(collection_driver, doc_id, consolidated_data2, 'teendriver')
+    # Gathering specific Document IDs for donors
+    collection_donor = "customer"
+    consolidated_data1 = []
+    collection_refA = db.collection(collection_donor)
+    document_idsA = [doc.id for doc in collection_refA.stream()]
 
-# Charitable Orgs
-for doc_id in document_idsC:
-    my_function(collection_depot, doc_id, consolidated_data3, 'org')
+    # Gathering specific Document IDs for teen drivers
+    collection_driver = "teendriver"
+    consolidated_data2 = []
+    collection_refB = db.collection(collection_driver)
+    document_idsB = [doc.id for doc in collection_refB.stream()]
+
+    consolidated_data3 = []
+    collection_depot = "depot"
+    collection_refC = db.collection(collection_depot)
+    document_idsC = [doc.id for doc in collection_refC.stream()]
+
+    # Donor Code
+    for doc_id in document_idsA:
+        read_collection(collection_donor, doc_id, consolidated_data1, 'customer')
+
+    # Teen Code
+    print(consolidated_data2)
+    for doc_id in document_idsB:
+        read_collection(collection_driver, doc_id, consolidated_data2, 'teendriver')
+    print(consolidated_data2)
+
+    # Charitable Orgs
+    for doc_id in document_idsC:
+        read_collection(collection_depot, doc_id, consolidated_data3, 'org')
+
+    routeplaninput['depots'] = consolidated_data3
+    routeplaninput['customers'] = consolidated_data1
+    routeplaninput['vehicles'] = consolidated_data2
+
+    #timefoldinput['customers'] = [{key: str(value) if key == 'id' else value for key, value in d.items()} for d in timefoldinput['customers']]
+    routeplaninput['depots'] = [{key: str(value) if key == 'id' else value for key, value in d.items()} for d in routeplaninput['depots']]
+    routeplaninput['vehicles'] = [{key: str(value) if key == 'depot' else value for key, value in d.items()} for d in routeplaninput['vehicles']]
+
+    for item in routeplaninput['customers']:
+        item['maxEndTime'] = item.pop('maxStartTime')
+
+    for item in routeplaninput['vehicles']:
+        item['departureTime'] = item.pop('departuretime')
+        item['arrivalTime'] = item.pop('arrivaltime')
 
 
-timefoldinput = {"name": "demo","southWestCorner": [36.044659, -80.244217], "northEastCorner": [36.099861,-79.766235],
-            "startDateTime": "2024-01-01T07:30:00", "endDateTime": "2024-02-28T00:00:00", 
-            "depots": [], "vehicles": [], "customers": [], "totalDrivingTimeSeconds": 0}
+#Main function for parsing routeplan output and writing to firebase
+def parse_routeplan_output(routeplanoutput):
 
-timefoldinput['depots'] = consolidated_data3
-timefoldinput['customers'] = consolidated_data1
-timefoldinput['vehicles'] = consolidated_data2
+    #with open('timefold_output2024-02-20-22-33-42.json') as outfile:
+    #    tout = json.load(outfile)
 
-#timefoldinput['customers'] = [{key: str(value) if key == 'id' else value for key, value in d.items()} for d in timefoldinput['customers']]
-timefoldinput['depots'] = [{key: str(value) if key == 'id' else value for key, value in d.items()} for d in timefoldinput['depots']]
-timefoldinput['vehicles'] = [{key: str(value) if key == 'depot' else value for key, value in d.items()} for d in timefoldinput['vehicles']]
+    for dep in routeplanoutput['depots']:
+        dep['address'] = reverse_loc(dep['location'])
 
-for item in timefoldinput['customers']:
-    item['maxEndTime'] = item.pop('maxStartTime')
+    for cust in routeplanoutput['customers']:
+        cust['address'] = reverse_loc(cust['location'])
 
-for item in timefoldinput['vehicles']:
-    item['departureTime'] = item.pop('departuretime')
-    item['arrivalTime'] = item.pop('arrivaltime')
+    vehicles = [veh for veh in routeplanoutput['vehicles'] if veh['customers']]
+    #vehicles2 = [veh for veh in vehicles1 if date_from_str(veh['departureTime']) <= routeplan_day]
 
-format = "%Y-%m-%d-%H-%M-%S"
-timefold_input_file = 'timefold_input'+datetime.now().strftime(format)+'.json'
-with open(timefold_input_file, 'w') as json_file:
-    json.dump(timefoldinput, json_file, indent=2)
+    routeplans = []
+    routeplan_day_str = routeplan_day.strftime('%b %d, %Y')
+    for veh in vehicles:
+        #print(veh)
+        depot = list(filter(lambda d: d['id'] == veh['depot'], routeplanoutput['depots']))
+        from_addr = depot[0]['address']
+        from_loc = GeoPoint(depot[0]['location'][0], depot[0]['location'][1])
+        driving_hours_earned = hr_min_from_seconds(veh['totalDrivingTimeSeconds'])
+        stop_num = 0
+        for cust_id in veh['customers']:
+            route_entry = {}
+            stop_num += 1
+            cust = list(filter(lambda d: d['id'] == cust_id, routeplanoutput['customers']))
+            stop_addr = cust[0]['address']
+            stop_loc = GeoPoint(cust[0]['location'][0], cust[0]['location'][1])
+            stop_name = cust[0]['name']
 
-with open('./timefoldParams.json') as outfile:
-    tfold_data = json.load(outfile)
+            route_entry = {'teenid': veh['id'], 'drivinghoursearned': driving_hours_earned, 'routecreateddt':routeplan_datetime,
+                        'StopNumber':stop_num, 'donorname': stop_name, 'ToAddress': stop_addr, 'ToLat': stop_loc,
+                        'FromAddress': from_addr, 'FromLat': from_loc, 'servicehoursearned': driving_hours_earned}
+            routeplans.append(route_entry)
 
-timefold_url = tfold_data['timefold_url']
-headers = {"Content-Type": "application/json", "Accept": "text/plain"}
-resp = requests.post(timefold_url, headers = headers, data=json.dumps(timefoldinput))
-timefold_jobid = str(resp.text)
-print(timefold_jobid)
+            from_addr = stop_addr
+            from_loc = stop_loc
+            #print(route_entry)
+        stop_num += 1
+        stop_addr = depot[0]['address']
+        stop_loc = GeoPoint(depot[0]['location'][0], depot[0]['location'][1])
+        route_entry = {'teenid': veh['id'], 'drivinghoursearned': driving_hours_earned, 'routecreateddt':routeplan_datetime,
+                        'StopNumber':stop_num, 'donorname': '', 'ToAddress': stop_addr, 'ToLat': stop_loc,
+                        'FromAddress': from_addr, 'FromLat': from_loc, 'servicehoursearned': driving_hours_earned}
+        routeplans.append(route_entry)
+
+    db = firestore.client()
+    rplan_collection = db.collection('routeplanui')
+    for rplan in routeplans:
+        doc_id = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20))
+        rplan_collection.document(doc_id).set(rplan)
 
 
-time.sleep(60)
 
-resp = requests.get(timefold_url + '/' + timefold_jobid, headers = {"Accept": "application/json"})
+app = Flask(__name__)
 
-timefold_output_file = 'timefold_output'+datetime.now().strftime(format)+'.json'
-out_file = open(timefold_output_file, 'w')
-json.dump(resp.json(), out_file, indent=2)
-out_file.close()
+@app.route("/", methods=["GET"])
+def create_routeplans():
+
+    routeplaninput = {"name": "demo","southWestCorner": [36.044659, -80.244217], "northEastCorner": [36.099861,-79.766235],
+                        "startDateTime": "2024-01-01T07:30:00", "endDateTime": "2024-02-28T00:00:00", 
+                        "depots": [], "vehicles": [], "customers": [], "totalDrivingTimeSeconds": 0}
+    read_routeplan_input(routeplaninput)
+
+    format = "%Y-%m-%d-%H-%M-%S"
+    routeplan_input_file = 'routeplan_input'+datetime.now().strftime(format)+'.json'
+    with open(routeplan_input_file, 'w') as json_file:
+        json.dump(routeplaninput, json_file, indent=2)
+
+
+    timefold_url = tfold_data['timefold_url']
+    headers = {"Content-Type": "application/json", "Accept": "text/plain"}
+    resp = requests.post(timefold_url, headers = headers, data=json.dumps(routeplaninput))
+    timefold_jobid = str(resp.text)
+    print(timefold_jobid)
+
+    time.sleep(60)
+
+    resp = requests.get(timefold_url + '/' + timefold_jobid, headers = {"Accept": "application/json"})
+    routeplanoutput = resp.json()
+
+    routeplan_output_file = 'timefold_output'+datetime.now().strftime(format)+'.json'
+    with open(routeplan_output_file, 'w') as out_file:
+        json.dump(routeplanoutput, out_file, indent=2)
+
+
+    parse_routeplan_output(routeplanoutput)
+    return(1)
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5051, debug=True)
+
